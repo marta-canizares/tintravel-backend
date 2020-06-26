a50b56eef9453e63d534df282c38ddfe9e8a0246
marta-canizares <marta.s.canizares@gmail.com>
2020-06-26 17:27:11 +0200

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+const path = require('path');
 import userRouter from './router/userRouter';
 import postsRouter from './router/postsRouter';
 import perfilRouter from './router/perfilRouter';
@@ -20,7 +22,12 @@ app.use(express.json())
 
 //app.use(express.static('Tintravel'))
 
-app.use('/', express.static(process.cwd() + '/Tintravel'))
+app.use(express.static(path.join(__dirname, 'Tintravel')));
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, 'Tintravel', 'index.html'));
+});
+
+//app.use('/', express.static(process.cwd() + '/Tintravel'))
 app.listen(80, () => console.log(`Front end app listening on port 80!`))
 
 
